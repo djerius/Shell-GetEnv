8621318a59893f85eca24a97f6f82e5e3748cb84
Diab Jerius <djerius@cfa.harvard.edu>
2016-07-25 10:38:39 -0400

    update logs and version for release

--- lib/Shell/GetEnv.pm
+++ lib/Shell/GetEnv.pm
@@ -30,7 +30,7 @@ use Carp;
 use File::Temp;
 use Shell::GetEnv::Dumper;
 
-our $VERSION = '0.08_06';
+our $VERSION = '0.09';
 
 my $status_var = 'p5_SHELL_GETENV_STATUS';
 
--- lib/Shell/GetEnv/Dumper.pm
+++ lib/Shell/GetEnv/Dumper.pm
@@ -6,7 +6,7 @@ use warnings;
 use Carp;
 use Storable;
 
-our $VERSION = '0.08_06';
+our $VERSION = '0.09';
 
 # crazy, but avoids need to use YAML or evals of Data::Dumper
 # or crazy shell escapes
